2b2529e6b90dcdecbd362fa0eba959e613c3627e
Stefan du Fresne <stefan@sdufresne.info>
2021-12-04 14:12:44 +0000

    Day 03 complete

--- src/test/scala/aoc2021/HelloSpec.scala
+++ src/test/scala/aoc2021/HelloSpec.scala
@@ -24,7 +24,29 @@ class Day02Spec extends AnyFunSuite {
 }
 
 class Day03Spec extends AnyFunSuite {
-  test("works") {
+
+  test("commonBitmask treats equal counts as 0 by default") {
+    assert(Day03.commonBitmask(List(
+      List(0,0,0),
+      List(1,1,1),
+    )) == List(0,0,0))
+  }
+
+  test("commonBitmask can treat equal counts as 0") {
+    assert(Day03.commonBitmask(List(
+      List(0,0,0),
+      List(1,1,1),
+    ), false) == List(0,0,0))
+  }
+
+  test("commonBitmask can treat equal counts as 1") {
+    assert(Day03.commonBitmask(List(
+      List(0,0,0),
+      List(1,1,1),
+    ), true) == List(1,1,1))
+  }
+
+  test("works part 1") {
     val testInput = List(
       "00100",
       "11110",
@@ -41,5 +63,24 @@ class Day03Spec extends AnyFunSuite {
     )
 
     assert(Day03.powerConsumption(testInput) == 198)
+  }
+
+  test("works part 2") {
+     val testInput = List(
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010",
+    )
+
+    assert(Day03.lifeSupportRating(testInput) == 230)
   }
 }

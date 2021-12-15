b237bc78242546a2813b731e5a84b1b517c38b24
Stefan du Fresne <stefan@sdufresne.info>
2021-12-15 14:09:43 +0000

    day 11 scaffolding

---
+++ src/main/scala/aoc2021/Day11.scala
@@ -0,0 +1,9 @@
+package aoc
+
+object Day11 {
+  def flashCount(input: String, steps: Int): Int = {
+    val array = input.split('\n').map(_.map(_.asDigit))
+
+    0
+  }
+}
---
+++ src/test/scala/aoc2021/Day11Spec.scala
@@ -0,0 +1,24 @@
+package aoc
+
+import org.scalatest.funsuite._
+import scala.io.Source
+
+class Day11Spec extends AnyFunSuite {
+  val testInput = """5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"""
+
+  test("part one works") {
+    assert(Day11.flashCount(testInput, 100) == 1656)
+  }
+  // test("completion score works") {
+  //   assert(Day10.completionScore(testInput) == 288957)
+  // }
+}
